35eebff23f78fc57c21d072a9cf7a1d428a26308
DFCU\stuart.smith <stuarts205@gmail.com>
2026-07-23 14:25:48 -0600

    04: add website and display websites

--- configs/schema.ts
+++ configs/schema.ts
@@ -1,6 +1,16 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const websiteTable = pgTable("websites", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    websiteId: varchar({ length: 255 }).notNull().unique(),
+    domain: varchar({ length: 255 }).notNull().unique(),
+    timezone: varchar({ length: 255 }).notNull(),
+    enableLocalhostTracking: boolean().default(false),
+    userEmail:varchar({ length: 255 }).notNull(),
 });
