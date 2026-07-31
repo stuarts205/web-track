7f51576897b286aa79dbd54c1b1529f5992f0dda
DFCU\stuart.smith <stuarts205@gmail.com>
2026-07-31 14:48:20 -0600

    14: deployment ready code

--- middleware.tsx
+++ middleware.tsx
@@ -3,7 +3,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
-    '/'
+    '/',
+    '/api/track',
+    '/api/live'
 ])
 
 export default clerkMiddleware(async (auth, req) => {
